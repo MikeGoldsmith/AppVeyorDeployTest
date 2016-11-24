e14af68bad0bd2dc01bc78969e95bbd6cf67baf8
MikeGoldsmith <goldsmith.mike@gmail.com>
2016-11-24 15:33:18 +0000

    add simple project with nuspec

---
+++ AppVeyorBuild/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AppVeyorBuild
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Logger
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Log(string text)
+        {
+            throw new NotImplementedException("Called Log");
+        }
+    }
+}
