c9509a1e4b6b667d9416c2ba78b4cf2223443c43
MikeGoldsmith <goldsmith.mike@gmail.com>
2016-11-30 16:51:58 +0000

    add test project

---
+++ AppVeyorBuildTests/LoggerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AppVeyorBuild;
+
+namespace AppVeyorBuildTests
+{
+    public class LoggerTests
+    {
+        public void Test()
+        {
+            var logger = new Logger();
+            logger.Log("test");
+        }
+    }
+}
