6f5cb215cdf316d223af3dfa7b8cf9033caa940a
MikeGoldsmith <goldsmith.mike@gmail.com>
2017-01-30 17:13:04 +0000

    switch to publickey for test project

--- AppVeyorBuild/Properties/AssemblyInfo.cs
+++ AppVeyorBuild/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("AppVeyorBuildTests, PublicKeyToken=d8afe804ad599344")]
+[assembly: InternalsVisibleTo("AppVeyorBuildTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000fdc804b64a4a111ca546ce4fa59ececb635c825460a04f4551973e0049d3b99712788638870eed8a6b3a05cb4a01ff6f1f8ebc0aea893fe22be6143409a61b09e1c3551aefe0715186d67234ebc381a198e8abaa74925fdda8c00ca4144b2fca11ed251bb5d3a497dc224abcedd4158252b959762134fd3f434142f61128d8b")]
