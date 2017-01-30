de0d1362fa963850f68a25c5d43f92f9366a75b1
MikeGoldsmith <goldsmith.mike@gmail.com>
2017-01-30 16:16:26 +0000

    only set internals on debug and add test assembly

--- AppVeyorBuild/Properties/AssemblyInfo.cs
+++ AppVeyorBuild/Properties/AssemblyInfo.cs
@@ -29,4 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
+
+#if DEBUG
 [assembly: InternalsVisibleTo("AppVeyorBuildTests")]
+#endif
