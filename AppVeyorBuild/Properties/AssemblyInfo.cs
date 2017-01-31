4fd717761a053d953fb41994dd3e886b7b15f01d
MikeGoldsmith <goldsmith.mike@gmail.com>
2017-01-31 12:25:29 +0000

    wrap internals visible to in debug flag

--- AppVeyorBuild/Properties/AssemblyInfo.cs
+++ AppVeyorBuild/Properties/AssemblyInfo.cs
@@ -30,4 +30,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
+#if DEBUG
 [assembly: InternalsVisibleTo("AppVeyorBuildTests")]
+#endif
